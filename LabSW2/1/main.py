9d5a1a6f1b4f58c0f04480b889de7547af2106c1
Matteo Quarta <matteo.quarta.carmiano@gmail.com>
2020-07-04 17:48:42 +0200

    Fixes after some testing

--- LabSW2/1/main.py
+++ LabSW2/1/main.py
@@ -10,8 +10,6 @@ PORT = 8080
 
 class BrokerInfo(object):
 	exposed=True
-	def __init__(self):
-		pass
 
 	@cherrypy.expose
 	def GET(self):
@@ -26,18 +24,18 @@ if __name__ == "__main__":
 	conf = {
 		'/' : {
 			'request.dispatch' : cherrypy.dispatch.MethodDispatcher(),
-			'tool.session.on' : True
 		}
 	}
 	
+	cherrypy.tree.mount(BrokerInfo(), '/broker_info', conf)
+	cherrypy.tree.mount(DeviceManager.DeviceManager(), '/devices', conf)
+	cherrypy.tree.mount(ServiceManager.ServiceManager(), '/services', conf)
+	cherrypy.tree.mount(UserManager.UserManager(), '/users', conf)
+
 	cherrypy.config.update({
 		'server.socket_host' : IP_ADDRESS,
 		'server.socket_port' : PORT
 		})
-	cherrypy.tree.mount(BrokerInfo(), '/broker_info')
-	cherrypy.tree.mount(DeviceManager.DeviceManager(), '/devices')
-	cherrypy.tree.mount(ServiceManager.ServiceManager(), '/services')
-	cherrypy.tree.mount(UserManager.UserManager(), '/users')
-	print("1")
+
 	cherrypy.engine.start()
 	cherrypy.engine.block()
