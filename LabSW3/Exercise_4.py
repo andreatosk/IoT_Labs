81a41b5ebf6e031486ca44048554ed39b8ac8548
pastarick <seric814@gmail.com>
2020-07-06 18:34:48 +0200

    added general error handling

--- LabSW3/Exercise_4.py
+++ LabSW3/Exercise_4.py
@@ -13,12 +13,19 @@ class MQTTPublisher():
         self.topics={}
 
         #registro al catalogo
-        self._register()
-        # ottengo info dal catalogo:
-        self._get_message_broker()
+        success=self._register()
 
+        if not success:
+            return("Failed to register to catalog.")
+        # ottengo info dal catalogo:
+        success=self._get_message_broker()
+
+        if not success:
+            return("Failed to retrieve Message Broker info.")
         #ottengo gli endpoints dal catalogo
-        self._get_topics()
+        success=self._get_topics()
+        if not success:
+            return("Failed to retrieve endpoints.")
 
         self.start()
 
@@ -30,38 +37,43 @@ class MQTTPublisher():
 
         r=requests.put(catalogURL, json.dumps(myself))
         if not r:
-            #4xx o 5xx
-            pass
+            return False
+        return True
 
     def _get_message_broker(self):
         r=requests.get(catalogURL)
         if not r:
-            #vuol dire che è stato ritornato un errore 4xx o 5xx
-            pass
+            return False
         data=json.loads(r.content)
         self.broker=data['ip_address']
         self.port=data['port']
+        return True
 
     def _get_topics(self):
         #la post senza parametri mi ritorna tutti i services del catalogo
         r=requests.post(self.catalogURL)
         if not r:
-            #4xx o 5xx
+            return False
         services=r.content
         for s_ID, service in services.items():
             #NON DOVREBBERO ESSERE TUTTI I TOPIC (?)
             self.topics[service['description']]=service['endpoints']
+        return True
 
     def start(self):
         self._mqtt.connect(self.broker, self.port)
         self._mqtt.loop_start()
 
+
+    #punto 1
     def actuate_fan(self):
         pass
 
+    #punto 2
     def actuate_led(self):
         pass
 
+    #punto 5
     def sense_people(self):
         #riceve info da PIR e noise
         pass
@@ -69,6 +81,7 @@ class MQTTPublisher():
     #punti 3 e 4 implementati nel 5
     #punto 6 non necessario
 
+    #punto 8
     def change_set_points(self, min_fan=None, max_fan=None, min_led=None, max_led=None):
         #cambia solo i setpoint effettivamente ricevuti
         #NB: PASSARE I PARAMETRI NELLA FORMA KEY=VALUE
